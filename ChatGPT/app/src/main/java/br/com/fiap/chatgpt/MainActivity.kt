10936131e2a7f38eb7a4e12ca3f95afeb46e05ca
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-03-27 20:58:28 -0300

    Aula 6 - Projeto ChatGpt pt 1

---
+++ ChatGPT/app/src/main/java/br/com/fiap/chatgpt/MainActivity.kt
@@ -0,0 +1,34 @@
+package br.com.fiap.chatgpt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.chatgpt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupButton()
+    }
+
+    private fun setupButton() {
+        binding.buttonGoToChat.setOnClickListener {
+            goToChatActivity()
+        }
+    }
+
+    private fun goToChatActivity() {
+        startActivity(
+            Intent(
+                this,
+                ChatActivity::class.java
+            )
+        )
+    }
+
+}
