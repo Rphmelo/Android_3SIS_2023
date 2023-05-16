4b7a3c8fad8675bd8b55595bc7afe325f3dadb6f
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-05-15 21:00:15 -0300

    Aula 12 - projeto countries parte 3

---
+++ Countries/app/src/main/java/br/com/fiap/countries/CountriesDataSource.kt
@@ -0,0 +1,8 @@
+package br.com.fiap.countries
+
+import br.com.fiap.countries.model.CountryModel
+
+object CountriesDataSource {
+
+    val countriesList: MutableList<CountryModel> = mutableListOf()
+}
