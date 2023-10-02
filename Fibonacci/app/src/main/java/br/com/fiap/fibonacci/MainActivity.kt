33e23e81f8120716c3b56d92b40c2662f2170db8
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-10-02 20:56:27 -0300

    Aula 20 - Corrotina na prática parte 2

--- Fibonacci/app/src/main/java/br/com/fiap/fibonacci/MainActivity.kt
+++ Fibonacci/app/src/main/java/br/com/fiap/fibonacci/MainActivity.kt
@@ -2,26 +2,44 @@ package br.com.fiap.fibonacci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import br.com.fiap.fibonacci.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<FibonacciViewModel>()
+    private var coroutine: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupButtons()
+        setObservers()
+    }
+
+    private fun setObservers() {
+        viewModel.fibonacciIsFinished.observe(this, Observer { fibonacciNumber ->
+            Snackbar.make(
+                binding.buttonIncrementFibonacci,
+                "$fibonacciNumber",
+                Snackbar.LENGTH_SHORT
+            ).show()
+        })
     }
 
     private fun incrementFibonacci(sequenceNumber: Int) {
-        lifecycleScope.launch(Dispatchers.Main) {
+        coroutine = lifecycleScope.launch(Dispatchers.Main) {
             binding.buttonIncrementFibonacci.isEnabled = false
-            val fibonacciNumber: Int = CoroutineFactory.calculateFibonacci(
+            val fibonacciNumber: Int = viewModel.calculateFibonacci(
                 sequenceNumber
             )
             binding.labelFibonacciNumber.text = "Valor do fibonacci: $fibonacciNumber"
@@ -35,6 +53,11 @@ class MainActivity : AppCompatActivity() {
             countClick++
             incrementFibonacci(countClick)
             binding.labelFibonacciSequence.text = "Sequencia de fibonacci: $countClick"
+        }
+
+        binding.buttonStopFibonacci.setOnClickListener {
+            binding.buttonIncrementFibonacci.isEnabled = true
+            coroutine?.cancel()
         }
     }
 }
