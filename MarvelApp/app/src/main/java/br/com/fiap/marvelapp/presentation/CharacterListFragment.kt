bc283856b7789bb51b6a925ef2b8cc32c6b7f57e
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-10-23 20:53:09 -0300

    Aula 22 - Retrofit e Marvel API parte 2

--- MarvelApp/app/src/main/java/br/com/fiap/marvelapp/presentation/CharacterListFragment.kt
+++ MarvelApp/app/src/main/java/br/com/fiap/marvelapp/presentation/CharacterListFragment.kt
@@ -5,15 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import br.com.fiap.marvelapp.databinding.FragmentCharacterListBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 
 class CharacterListFragment : Fragment() {
 
     private lateinit var binding: FragmentCharacterListBinding
+    private val viewModel: CharacterListViewModel by viewModels()
 
-//    private val characterListAdapter by lazy {
-//        CharacterListAdapter()
-//    }
+    private val characterListAdapter by lazy {
+        CharacterListAdapter()
+    }
+
+    private fun getMarvelCharacters() {
+        lifecycleScope.launch {
+            viewModel.listCharacters()
+        }
+    }
+
+    private fun setObservers() {
+        viewModel.characterErrorState.observe(
+            viewLifecycleOwner,
+            Observer {
+                Snackbar.make(
+                    binding.recyclerViewCharacters,
+                    it,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+                binding.characterLoading.isVisible = false
+                binding.buttonTryAgain.isVisible = true
+                binding.buttonTryAgain.setOnClickListener {
+                    lifecycleScope.launch {
+                        viewModel.listCharacters()
+                    }
+                }
+            }
+        )
+        viewModel.characterSuccessState.observe(
+            viewLifecycleOwner,
+            Observer {
+                binding.buttonTryAgain.isVisible = false
+                binding.characterLoading.isVisible = false
+                binding.recyclerViewCharacters.isVisible = true
+                characterListAdapter.setData(it)
+            }
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,11 +68,13 @@ class CharacterListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
+        setObservers()
+        getMarvelCharacters()
     }
 
     private fun setupRecyclerView() {
         binding.recyclerViewCharacters.setHasFixedSize(true)
-//        binding.recyclerViewCharacters.adapter = characterListAdapter
+        binding.recyclerViewCharacters.adapter = characterListAdapter
     }
 
 }
